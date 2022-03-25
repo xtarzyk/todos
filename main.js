ef4d4a06538b7a87e2e69a73c8ffd98a2fdb7217
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2022-03-25 20:47:04 +0100

    task-1 done

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-import './style.css'
+const addTaskButton = document.querySelector('.header__button')
+const taskName = document.querySelector('.header__input')
+const mainList = document.querySelector('.main__list')
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+addTaskButton.addEventListener('click', () => {
+    const newDiv = document.createElement('div')
+    const newListItem = document.createElement('li')
+    const newTrash = document.createElement('span')
+
+    newTrash.innerHTML += '<i class="fa-solid fa-trash-can"></i>'
+    newDiv.classList.add('list-item')
+    newListItem.classList.add('list-item__li-tag')
+    newListItem.textContent = `${taskName.value}`
+
+    mainList.append(newDiv)
+    newDiv.append(newListItem)
+    newDiv.append(newTrash)
+})
+
+mainList.addEventListener('click', event => {
+    if (event.target.tagName === 'LI') {
+        event.target.style.textDecoration = 'line-through'
+    }
+    if (event.target.className === 'fa-solid fa-trash-can') {
+        event.target.parentNode.parentNode.remove()
+    }
+})

a0b9e75c59a1f33a2541404aaf643f1410ecf738
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2022-03-26 14:17:34 +0100

    task-2 almost done, event listeners needs to be remove

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
 const addTaskButton = document.querySelector('.header__button')
 const taskName = document.querySelector('.header__input')
 const mainList = document.querySelector('.main__list')
+const trashIcon = '<i class="fa-solid fa-trash-can"></i>'
+const pencilIcon = '<i class="fa-solid fa-pencil"></i>'
 
 
 addTaskButton.addEventListener('click', () => {
     const newDiv = document.createElement('div')
     const newListItem = document.createElement('li')
-    const newTrash = document.createElement('span')
+    const newSpan = document.createElement('span')
 
-    newTrash.innerHTML += '<i class="fa-solid fa-trash-can"></i>'
+    newSpan.innerHTML += pencilIcon + trashIcon
     newDiv.classList.add('list-item')
     newListItem.classList.add('list-item__li-tag')
     newListItem.textContent = `${taskName.value}`
 
     mainList.append(newDiv)
     newDiv.append(newListItem)
-    newDiv.append(newTrash)
+    newDiv.append(newSpan)
 })
 
-mainList.addEventListener('click', event => {
+function editListItem(event) {
+    const li = event.target.parentNode.previousSibling
+    taskName.addEventListener('change', e => {
+        li.textContent = e.target.value
+    })
+    mainList.removeEventListener('click', handleChange)
+    taskName.removeEventListener('change', editListItem)
+}
+
+function handleChange(event) {
     if (event.target.tagName === 'LI') {
         event.target.style.textDecoration = 'line-through'
+    }
+    if (event.target.className === 'fa-solid fa-pencil') {
+        editListItem(event);
     }
     if (event.target.className === 'fa-solid fa-trash-can') {
         event.target.parentNode.parentNode.remove()
     }
+    mainList.removeEventListener('click', handleChange)
+}
+
+
+mainList.addEventListener('click', event => {
+    handleChange(event);
 })
+
+
+

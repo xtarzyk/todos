785b967ed86d6709e5fb24bd8c4bb991a41ce094
xtarzyk <xtarzyk@tutanota.com>
2022-03-28 16:16:16 +0200

    monday morning was hard

--- main.js
+++ main.js
@@ -1,60 +1,108 @@
 const addTaskButton = document.querySelector('.header__button')
 const taskName = document.querySelector('.header__input')
 const mainList = document.querySelector('.main__list')
-const trashIcon = '<i class="fa-solid fa-trash-can"></i>'
-const pencilIcon = '<i class="fa-solid fa-pencil"></i>'
 const radioBtns = document.querySelectorAll('input[name="filter"]')
+let todos = []
 
 
-addTaskButton.addEventListener('click', () => {
-        const newDiv = document.createElement('div')
-        const newListItem = document.createElement('li')
-        const newSpan = document.createElement('span')
+const createTask = () => {
+    const newId = Date.now().toString()
+    const newDiv = document.createElement('div')
+    const newListItem = createNewListItem(newId)
+    const newSpan = createSpanBtns(newId)
 
-        newSpan.innerHTML = pencilIcon + trashIcon
-        newDiv.classList.add('list-item')
-        newListItem.classList.add('list-item__li-tag')
-        newListItem.textContent = `${taskName.value}`
+    newDiv.classList.add('list-item')
+    newListItem.classList.add('list-item__li-tag')
+    newListItem.textContent = taskName.value
 
-        mainList.append(newDiv)
-        newDiv.append(newListItem)
-        newDiv.append(newSpan)
-})
+    newDiv.append(newListItem)
+    newDiv.append(newSpan)
+    todos = todos.concat({
+        text: taskName.value,
+        isDone: false,
+        id: newId
+    })
+    newDiv.setAttribute("todo-id", newId.toString())
+    mainList.append(newDiv)
+    taskName.value = ""
+    console.log(todos)
+    console.log(newDiv)
+}
 
-const editListItem = event => {
-    const li = event.target.parentNode.previousSibling
-    taskName.addEventListener('change', e => {
-        li.textContent = e.target.value
+const createNewListItem = id => {
+    const newListItem = document.createElement('li')
+    newListItem.addEventListener('click', event => strikeOut(event, id))
+
+    return newListItem
+}
+
+
+const strikeOut = (event, id) => {
+    event.target.style.textDecoration = 'line-through'
+    todos = todos.map(todo => {
+        if (id === todo.id) {
+            todo.isDone = true
+            return todo
+        }
+        return todo
     })
 }
 
-const handleChange = event => {
-    if (event.target.tagName === 'LI') {
-        event.target.style.textDecoration = 'line-through'
-    }
-    if (event.target.className === 'fa-solid fa-pencil') {
-        editListItem(event);
-    }
-    if (event.target.className === 'fa-solid fa-trash-can') {
+const createSpanBtns = id => {
+    const span = document.createElement('span')
+    const trashIcon = document.createElement('i')
+    const pencilIcon = document.createElement('i')
+
+    trashIcon.classList.add("fa-solid", "fa-trash-can")
+    pencilIcon.classList.add("fa-solid", "fa-pencil")
+
+    trashIcon.addEventListener('click', event => {
         event.target.parentNode.parentNode.remove()
-    }
+        todos = todos.filter(element => element.id === id)
+    })
+    pencilIcon.addEventListener('click', event => {
+        // span.style.display = 'none'
+        event.stopPropagation()
+        editListItem(event, id)
+    })
+    span.append(pencilIcon, trashIcon)
+    return span
 }
+
+const editListItem = (event, id) => {
+    const li = event.target.parentNode.previousSibling
+    const editionInput = document.createElement('input')
+    li.textContent = ''
+    editionInput.classList.add('edition-input')
+    li.appendChild(editionInput)
+    editionInput.addEventListener('change', editionEvent => {
+        editionEvent.stopPropagation()
+
+        li.textContent = editionEvent.target.value
+        todos = todos.map(todo => {
+            if (id === todo.id) {
+                todo.text = li.textContent
+                return todo
+            }
+            return todo
+        })
+    })
+
+}
+
 
 const filterList = event => {
     const currentNodeList = document.querySelectorAll('.list-item__li-tag')
     const currentList = Array.from(currentNodeList)
-    if(event.target.id === 'todo') {
+    if (event.target.id === 'todo') {
         let noLineList = currentList.filter(li => li.style.textDecoration !== 'line-through')
         console.log(noLineList)
     }
 }
 
+addTaskButton.addEventListener('click', createTask)
 
-mainList.addEventListener('click', event => {
-    handleChange(event)
-})
-
-radioBtns.forEach( btn => {
+radioBtns.forEach(btn => {
     btn.addEventListener('change', event => filterList(event))
 })
 

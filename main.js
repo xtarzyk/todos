a1cc49118b390e0c92d12e5a09deb698596ea8d0
xtarzyk <xtarzyk@tutanota.com>
2022-04-21 16:31:46 +0200

    changing functions location to the class, edition and removing items does not work

--- main.js
+++ main.js
@@ -5,42 +5,17 @@ const taskName = document.querySelector('.header__input')
 const mainList = document.querySelector('.main__list')
 const radioBtns = document.querySelectorAll('input[name="filter"]')
 let todos = []
-const todo = new Todo
-
 
 const createTask = () => {
-    const newId = todo.getId() /*Date.now().toString()*/
-    const newDiv = document.createElement('div')
-    const newListItem = createNewListItem(newId)
-    const newSpan = createSpanBtns(newId)
+    const todo = new Todo(taskName.value)
+    const newId = todo.getId()
+    const newTodo = todo.render(strikeOutAndReverse, todos, taskName)
 
-    newDiv.classList.add('list-item')
-    newListItem.classList.add('list-item__li-tag')
-    newListItem.textContent = taskName.value
-
-    newDiv.append(newListItem)
-    newDiv.append(newSpan)
     todos = todos.concat(todo)
-    // {
-        // text: taskName.value,
-        // isDone: false,
-        // id: newId
-    // }
-    // )
-    newDiv.setAttribute("todo-id", newId)
-    mainList.append(newDiv)
-    taskName.value = ""
-}
-
-const createNewListItem = id => {
-    const newListItem = document.createElement('li')
-
-    newListItem.addEventListener('click', event => {
-        if (newListItem.textContent !== "") {
-            strikeOutAndReverse(event, id)
-        }
-    })
-    return newListItem
+    newTodo.setAttribute('todo-id', newId)
+    mainList.append(newTodo)
+    console.log(todos)
+    taskName.value = ''
 }
 
 
@@ -53,78 +28,77 @@ const strikeOutAndReverse = (event, id) => {
         event.target.style.textDecoration = 'line-through'
         editIcon.style.display = 'none'
     }
-
-    todos = todos.map(todo => {
-        if (id === todo.id) {
-            return {
-                ...todo,
-                isDone: !todo.isDone,
-            }
+    console.log(todos)
+    todos.forEach(todo => {
+        if (id === todo.getId()) {
+            todo.setIsDone(!todo.getIsDone())
         }
-        return todo
     })
 }
 
-const createSpanBtns = id => {
-    const span = document.createElement('span')
-    const trashIcon = document.createElement('i')
-    const pencilIcon = document.createElement('i')
+// const createSpanBtns = id => {
+//     const span = document.createElement('span')
+//     const trashIcon = document.createElement('i')
+//     const pencilIcon = document.createElement('i')
+//
+//     trashIcon.classList.add('fa-solid', 'fa-trash-can')
+//     pencilIcon.classList.add('fa-solid', 'fa-pencil')
+//
+//     trashIcon.addEventListener('click', event => {
+//         event.target.parentNode.parentNode.remove()
+//         todos = todos.filter(element => element.getId() !== id)
+//     })
+//     pencilIcon.addEventListener('click', event => {
+//         const currentDiv = event.target.parentNode.parentNode
+//         const currentListItem = event.target.parentNode.previousSibling
+//
+//         currentDiv.classList.remove('list-item')
+//         currentListItem.classList.remove('list-item__li-tag')
+//         currentDiv.classList.add('edited-list-item')
+//         currentListItem.classList.add('edited-list-item__li-tag')
+//
+//         span.style.display = 'none'
+//         editListItem(event, id)
+//     })
+//     span.append(pencilIcon, trashIcon)
+//     return span
+// }
 
-    trashIcon.classList.add('fa-solid', 'fa-trash-can')
-    pencilIcon.classList.add('fa-solid', 'fa-pencil')
-
-    trashIcon.addEventListener('click', event => {
-        event.target.parentNode.parentNode.remove()
-        todos = todos.filter(element => element.id !== id)
-    })
-    pencilIcon.addEventListener('click', event => {
-        const currentDiv = event.target.parentNode.parentNode
-        const currentListItem = event.target.parentNode.previousSibling
-
-        currentDiv.classList.remove('list-item')
-        currentListItem.classList.remove('list-item__li-tag')
-        currentDiv.classList.add('edited-list-item')
-        currentListItem.classList.add('edited-list-item__li-tag')
-
-        span.style.display = 'none'
-        editListItem(event, id)
-    })
-    span.append(pencilIcon, trashIcon)
-    return span
-}
-
-const editListItem = (event, id) => {
-    const li = event.target.parentNode.previousSibling
-    const div = li.parentNode
-    const editionInput = document.createElement('input')
-    const span = event.target.parentNode
-
-    li.textContent = ''
-    editionInput.classList.add('edition-input')
-    li.appendChild(editionInput)
-
-    editionInput.addEventListener('change', editionEvent => {
-        li.textContent = editionEvent.target.value
-        todos = todos.map(todo => {
-            if (id === todo.id) {
-                todo.text = li.textContent
-                return todo
-            }
-            return todo
-        })
-        div.classList.remove('edited-list-item')
-        li.classList.remove('edited-list-item__li-tag')
-        div.classList.add('list-item')
-        li.classList.add('list-item__li-tag')
-        span.style.display = 'block'
-    })
-}
-
-
+// const editListItem = (event, id) => {
+//     const li = event.target.parentNode.previousSibling
+//     const div = li.parentNode
+//     const editionInput = document.createElement('input')
+//     const span = event.target.parentNode
+//
+//     li.textContent = ''
+//     editionInput.classList.add('edition-input')
+//     li.appendChild(editionInput)
+//
+//     editionInput.addEventListener('change', editionEvent => {
+//         li.textContent = editionEvent.target.value
+//         todos = todos.map(todo => {
+//             if (id === todo.getId()) {
+//                 // todo.text = li.textContent
+//                 todo.setText(li.textContent)
+//                 console.log(todo)
+//                 return todo
+//             }
+//             return todo
+//         })
+//         div.classList.remove('edited-list-item')
+//         li.classList.remove('edited-list-item__li-tag')
+//         div.classList.add('list-item')
+//         li.classList.add('list-item__li-tag')
+//         span.style.display = 'block'
+//     })
+// }
+//
+//
 const filterList = event => {
     const currentNodeList = document.querySelectorAll('.list-item')
-    const finishedTasksList = todos.filter(todo => todo.isDone)
-    const finishedId = finishedTasksList.map(task => task.id)
+    console.log(todos)
+    const finishedTasksList = todos.filter(todo => todo.getIsDone())
+    const finishedId = finishedTasksList.map(task => task.getId())
 
     if (event.target.id === 'todo') {
         currentNodeList.forEach(div => {
--- todo.js
+++ todo.js
@@ -5,10 +5,10 @@ export class Todo {
     #isDone
     #text
 
-    constructor() {
+    constructor(text) {
         this.#id = uuidv4()
         this.#isDone = false
-        this.#text = ''
+        this.#text = text
     }
 
     getId() {
@@ -23,11 +23,90 @@ export class Todo {
         this.#isDone = value
     }
 
-    getText() {
-        return this.#text
-    }
-
     setText(value) {
         this.#text = value
+    }
+
+    render(onClick, todos, taskName) {
+        const newDiv = document.createElement('div')
+        const newListItem = this.#createNewListItem(onClick)
+        const newSpan = this.#createSpanBtns(todos)
+
+        newDiv.classList.add('list-item')
+        newListItem.classList.add('list-item__li-tag')
+        newListItem.textContent = taskName.value
+        newDiv.append(newListItem)
+        newDiv.append(newSpan)
+
+        return newDiv
+    }
+
+    #createNewListItem(onClick) {
+        const newListItem = document.createElement('li')
+
+        newListItem.addEventListener('click', event => {
+            if (newListItem.textContent !== '') {
+                onClick(event, this.#id)
+            }
+        })
+
+        return newListItem
+    }
+
+    #createSpanBtns(todos) {
+        const span = document.createElement('span')
+        const trashIcon = document.createElement('i')
+        const pencilIcon = document.createElement('i')
+
+        trashIcon.classList.add('fa-solid', 'fa-trash-can')
+        pencilIcon.classList.add('fa-solid', 'fa-pencil')
+
+        trashIcon.addEventListener('click', event => {
+            event.target.parentNode.parentNode.remove()
+            todos = todos.filter(element => element.getId() !== this.#id)
+        })
+        pencilIcon.addEventListener('click', event => {
+            const currentDiv = event.target.parentNode.parentNode
+            const currentListItem = event.target.parentNode.previousSibling
+
+            currentDiv.classList.remove('list-item')
+            currentListItem.classList.remove('list-item__li-tag')
+            currentDiv.classList.add('edited-list-item')
+            currentListItem.classList.add('edited-list-item__li-tag')
+
+            span.style.display = 'none'
+            this.#editListItem(event, todos)
+        })
+        span.append(pencilIcon, trashIcon)
+        return span
+    }
+
+    #editListItem(event, todos) {
+        const li = event.target.parentNode.previousSibling
+        const div = li.parentNode
+        const editionInput = document.createElement('input')
+        const span = event.target.parentNode
+
+        li.textContent = ''
+        editionInput.classList.add('edition-input')
+        li.appendChild(editionInput)
+
+        editionInput.addEventListener('change', editionEvent => {
+            li.textContent = editionEvent.target.value
+            todos = todos.map(todo => {
+                if (this.#id === todo.getId()) {
+                    // todo.text = li.textContent
+                    todo.setText(li.textContent)
+                    console.log(todo)
+                    return todo
+                }
+                return todo
+            })
+            div.classList.remove('edited-list-item')
+            li.classList.remove('edited-list-item__li-tag')
+            div.classList.add('list-item')
+            li.classList.add('list-item__li-tag')
+            span.style.display = 'block'
+        })
     }
 }

ca22385fcc706b0fa576ee815eb53829e4fec47c
xtarzyk <xtarzyk@tutanota.com>
2022-04-25 10:35:02 +0200

    another step to perfection!

--- main.js
+++ main.js
@@ -8,51 +8,29 @@ let todos = []
 
 const createTask = () => {
     const todo = new Todo(taskName.value)
-    const newTodo = todo.render(strikeOutAndReverse, createSpanBtns)
+    const newTodo = todo.render(strikeOutAndReverse, deleteItem, editListItem)
 
     todos = todos.concat(todo)
     mainList.append(newTodo)
     taskName.value = ''
 }
 
-const strikeOutAndReverse = event => {
-    const editIcon = event.target.nextSibling.firstChild
-
-    if (event.target.style.textDecoration === 'line-through') {
-        event.target.style.textDecoration = 'none'
+const strikeOutAndReverse = (editIcon, textNode) => {
+    if (textNode.style.textDecoration === 'line-through') {
+        textNode.style.textDecoration = 'none'
         editIcon.style.display = 'inline-block'
-    } else {
-        event.target.style.textDecoration = 'line-through'
-        editIcon.style.display = 'none'
+
+        return
     }
+        textNode.style.textDecoration = 'line-through'
+        editIcon.style.display = 'none'
 }
 
-const createSpanBtns = id => {
-    const span = document.createElement('span')
-    const trashIcon = document.createElement('i')
-    const pencilIcon = document.createElement('i')
-
-    trashIcon.classList.add('fa-solid', 'fa-trash-can')
-    pencilIcon.classList.add('fa-solid', 'fa-pencil')
-
+const deleteItem = (trashIcon, id) => {
     trashIcon.addEventListener('click', event => {
         event.target.parentNode.parentNode.remove()
         todos = todos.filter(element => element.getId() !== id)
     })
-    pencilIcon.addEventListener('click', event => {
-        const currentDiv = event.target.parentNode.parentNode
-        const currentListItem = event.target.parentNode.previousSibling
-
-        currentDiv.classList.remove('list-item')
-        currentListItem.classList.remove('list-item__li-tag')
-        currentDiv.classList.add('edited-list-item')
-        currentListItem.classList.add('edited-list-item__li-tag')
-
-        span.style.display = 'none'
-        editListItem(event, id)
-    })
-    span.append(pencilIcon, trashIcon)
-    return span
 }
 
 const editListItem = (event, id) => {
@@ -86,26 +64,31 @@ const editListItem = (event, id) => {
 
 const filterList = event => {
     const currentNodeList = document.querySelectorAll('.list-item')
-    const finishedTasksList = todos.filter(todo => todo.getIsDone())
-    const finishedId = finishedTasksList.map(task => task.getId())
+    const finishedTasksIds = todos
+        .filter(todo => todo.getIsDone())
+        .map(task => task.getId())
 
     if (event.target.id === 'todo') {
         currentNodeList.forEach(div => {
-            if (finishedId.includes(div.getAttribute('todo-id'))) {
+            if (finishedTasksIds
+                .includes(div.getAttribute('todo-id'))) {
                 div.style.display = 'none'
-            } else {
-                div.style.display = 'flex'
+
+                return
             }
+                div.style.display = 'flex'
         })
     }
 
     if (event.target.id === 'strokes') {
         currentNodeList.forEach(div => {
-            if (!finishedId.includes(div.getAttribute('todo-id'))) {
+            if (!finishedTasksIds
+                .includes(div.getAttribute('todo-id'))) {
                 div.style.display = 'none'
-            } else {
-                div.style.display = 'flex'
+
+                return
             }
+                div.style.display = 'flex'
         })
     }
 
@@ -119,5 +102,7 @@ addTaskButton.addEventListener('click', createTask)
 radioBtns.forEach(btn => {
     btn.addEventListener('change', event => filterList(event))
 })
+
+
 
 
--- todo.js
+++ todo.js
@@ -4,6 +4,7 @@ export class Todo {
     #id
     #isDone
     #text
+    #editIcon
 
     constructor(text) {
         this.#id = uuidv4()
@@ -27,10 +28,10 @@ export class Todo {
         this.#text = value
     }
 
-    render(onClick, createSpanBtns) {
+    render(onClick, deleteItem, editListItem) {
         const newDiv = document.createElement('div')
         const newListItem = this.#createNewListItem(onClick)
-        const newSpan = createSpanBtns(this.#id)
+        const newSpan = this.#createSpanBtns(deleteItem, editListItem)
 
         newDiv.classList.add('list-item')
         newDiv.setAttribute('todo-id', this.#id)
@@ -47,7 +48,7 @@ export class Todo {
 
         newListItem.addEventListener('click', event => {
             if (newListItem.textContent !== '') {
-                onClick(event, this.#id)
+                onClick(this.#editIcon, newListItem)
                 this.toggleIsDone()
             }
         })
@@ -55,4 +56,32 @@ export class Todo {
         return newListItem
     }
 
+    #createSpanBtns(deleteItem, editListItem) {
+        const span = document.createElement('span')
+        const trashIcon = document.createElement('i')
+        const pencilIcon = document.createElement('i')
+    
+        trashIcon.classList.add('fa-solid', 'fa-trash-can')
+        pencilIcon.classList.add('fa-solid', 'fa-pencil')
+    
+        deleteItem(trashIcon, this.#id)
+    
+        pencilIcon.addEventListener('click', event => {
+            const currentDiv = event.target.parentNode.parentNode
+            const currentListItem = event.target.parentNode.previousSibling
+    
+            currentDiv.classList.remove('list-item')
+            currentListItem.classList.remove('list-item__li-tag')
+            currentDiv.classList.add('edited-list-item')
+            currentListItem.classList.add('edited-list-item__li-tag')
+    
+            span.style.display = 'none'
+            editListItem(event, this.#id)
+        })
+    
+        span.append(pencilIcon, trashIcon)
+        this.#editIcon = pencilIcon
+    
+        return span
+    }
 }
